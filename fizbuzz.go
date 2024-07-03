05d6596650aaad438154eaa87bcb1f2b930cf143
flanker0507 <108620970+flanker0507@users.noreply.github.com>
2024-07-04 06:58:41 +0700

    FizzBuzz

--- fizbuzz.go
+++ fizbuzz.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func FizzBuzz(n int) {
 	for i := 1; i <= n; i++ {
-		if i%5 == 0 && i%3 == 0 {
+		if i%3 == 0 && i%5 == 0 {
 			fmt.Println("FizzBuzz")
-		} else if i%5 == 0 {
-			fmt.Println("Fizz")
 		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
 			fmt.Println("Buzz")
 		} else {
 			fmt.Println(i)
